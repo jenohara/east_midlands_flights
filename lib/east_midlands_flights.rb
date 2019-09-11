7b315da11786c74a5176e1b8670ca50075b382a6
LadyWitch <jenny.s.ohara@gmail.com>
2019-09-11 21:16:15 +0100

    first commit

---
+++ lib/east_midlands_flights.rb
@@ -0,0 +1,9 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+require 'colorize'
+
+require_relative "./east_midlands_flights/cli"
+require_relative "./east_midlands_flights/flights"
+require_relative "./east_midlands_flights/scraper"
+require_relative "./east_midlands_flights/version"
---
+++ lib/east_midlands_flights/version.rb
@@ -0,0 +1,3 @@
+module EastMidlandsFlights
+  VERSION = "0.1.0"
+end
